d8fe268175b17feeb63a75725bb859971d128f57
Austin Howard <cnfishead@hotmail.com>
2021-11-22 11:55:14 -0500

    ...

--- routes/bootcampRoutes.js
+++ routes/bootcampRoutes.js
@@ -17,6 +17,7 @@ const router = express.Router();
 // Include other routers
 import courseRouter from "./courseRoutes.js";
 import reviewRouter from "./reviewRoutes.js";
+
 // Re-route into other resource routers
 router.use("/:bootcampId/courses", courseRouter);
 router.use("/:bootcampId/reviews", reviewRouter);
